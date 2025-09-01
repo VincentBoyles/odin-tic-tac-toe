35b8a84f2b4f5a4187f4b2e0ecdea43d8ef6bf49
VincentBoyles <boylesvincentmanuel@gmail.com>
2025-09-02 00:37:58 +0800

    privatized methods

--- lib/game.rb
+++ lib/game.rb
@@ -9,31 +9,6 @@ class Game
     @player2 = ''
   end
 
-  def ask_name
-    input_choice = ''
-    until input_choice != ''
-      puts 'Please enter your name: '
-      input_choice = gets.chomp
-    end
-    input_choice
-  end
-
-  def ask_piece
-    input_choice = ''
-    until %w[X O].include?(input_choice)
-      puts 'Please choose between X and O.'
-      input_choice = gets.chomp
-    end
-    input_choice
-  end
-
-  def register_players
-    player1 = HumanPlayer.new(ask_name, ask_piece)
-    player2 = ComputerPlayer.new(player1.piece == 'X' ? 'O' : 'X')
-    self.player1 = player1
-    self.player2 = player2
-  end
-
   def game_start
     puts 'Welcome to Tic-Tac-Toe!'
     register_players
@@ -46,11 +21,36 @@ class Game
       gameboard.computer_place_piece(player2.piece)
       puts "#{player2.name}'s turn" + gameboard.show_board
       break if gameboard.check_winner(player2.piece)
-
-      # gameboard.check_winner(player1.piece) || gameboard.check_winner(player2.piece) || gameboard.tiles.empty?
     end
     puts "#{player1.name} wins!" if gameboard.check_winner(player1.piece)
     puts "#{player2.name} wins!" if gameboard.check_winner(player2.piece)
     puts gameboard.show_board
+  end
+
+  private
+
+  def register_players
+    player1 = HumanPlayer.new(ask_name, ask_piece)
+    player2 = ComputerPlayer.new(player1.piece == 'X' ? 'O' : 'X')
+    self.player1 = player1
+    self.player2 = player2
+  end
+
+  def ask_piece
+    input_choice = ''
+    until %w[X O].include?(input_choice)
+      puts 'Please choose between X and O.'
+      input_choice = gets.chomp
+    end
+    input_choice
+  end
+
+  def ask_name
+    input_choice = ''
+    until input_choice != ''
+      puts 'Please enter your name: '
+      input_choice = gets.chomp
+    end
+    input_choice
   end
 end
